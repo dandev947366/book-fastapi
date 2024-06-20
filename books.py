e2fa9c7fc77d04eb5e6db8fb64ef6554a6786552
dandev947366 <dan.dev947366@gmail.com>
2024-06-20 19:29:46 +0300

    update endpoints

--- books.py
+++ books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import Body, FastAPI, Query
 
 app = FastAPI()
 
@@ -33,3 +33,19 @@ async def read_books_by_author_and_category(author: str = Query(None), category:
     else:
         return {"error": "Provide author and/or category"}
     return books_to_return
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+    
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+            
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
